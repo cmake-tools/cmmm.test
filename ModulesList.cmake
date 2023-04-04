eb7300caad7257772cf0896c872c54bafa6d83aa
flagarde <flagarde.programs@pm.me>
2023-04-04 17:21:27 +0800

    Update ModulesList.cmake

--- ModulesList.cmake
+++ ModulesList.cmake
@@ -1,2 +1,2 @@
-cmmm_define_module(Example1 LOCALE /example1/Example1.cmake)
-cmmm_define_module(Example2 LOCALE /example1/Example2.cmake)
+cmmm_define_module(Example1 LOCAL /example1/Example1.cmake)
+cmmm_define_module(Example2 LOCAL /example1/Example2.cmake)
